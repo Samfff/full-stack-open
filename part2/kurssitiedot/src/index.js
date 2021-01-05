159482d418130eb7197031e8bceff90b58ef67ef
Sam <samuel.sihvonen@aalto.fi>
2021-01-05 14:56:30 +0200

    Cloned kurssitiedot to part 2

---
+++ part2/kurssitiedot/src/index.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+      <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0]}/>
+      <Part part={props.parts[1]}/>
+      <Part part={props.parts[2]}/>
+    </div>
+  )
+}
+
+const Total = (props) => {
+    let exercisesTotal = 0
+    props.parts.forEach(part => exercisesTotal = (exercisesTotal + part.exercises))
+    return (
+      <p>
+        Number of exercises {exercisesTotal}
+      </p>
+    )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts}/>
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
