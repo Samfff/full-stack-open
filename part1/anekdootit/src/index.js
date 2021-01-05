7e662e37ac55fd4abeadbeafeb85e4ec3cbc1ba7
Sam <samuel.sihvonen@aalto.fi>
2021-01-05 14:35:25 +0200

    1.12 & 1.13 done

--- part1/anekdootit/src/index.js
+++ part1/anekdootit/src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = (props) => {
+
+  const random = (list) => (
+    Math.floor(Math.random() * list.length)
+  )
+
+  const [votes, setVotes] = useState(new Array(props.anecdotes.length).fill(0))
+  const [selected, setSelected] = useState(0)
+
+  const nextAnecdote = () => {
+    setSelected(random(props.anecdotes))
+  }
+
+  const vote = () => {
+    const copy = [...votes]
+    copy[selected] = copy[selected] + 1
+    setVotes(copy)
+  }
+
+  return (
+    <div>
+      <div>
+        <p>{props.anecdotes[selected]} </p>
+        <p> has {votes[selected]} votes </p>
+    </div>
+    <button onClick={nextAnecdote}>next anecdote</button>
+    <button onClick={vote}>vote</button>
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
