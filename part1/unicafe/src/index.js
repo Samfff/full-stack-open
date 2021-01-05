93ba6a55ebd9e4dba7a7a32e7ae067580f5909ab
Sam <samuel.sihvonen@aalto.fi>
2021-01-05 14:03:05 +0200

    1.11 done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -5,22 +5,29 @@ const Button = ({text, handleClick}) => (
   <button onClick={handleClick}> {text} </button>
 )
 
-const StatisticsLine = ({text, value}) => (
-  <p>{text} {value}</p>
-)
+const StatisticsLine = ({text, value}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
 
 const Average = ({good, neutral, bad}) => {
   const n = good + neutral + bad
   const average = ( (good * 1) + (neutral * 0) + (bad * -1) ) / n
   return (
-    <p>average {average}</p>
+    (<p>average {average}</p> )
   )
 }
 
 const Positive = ({good, neutral, bad}) => {
   const n = good + neutral + bad
   const positivePercent = (good / n)
-  return (<p>positive {100 * positivePercent} %</p>)
+  return (
+    (<p>positive {positivePercent} %</p>)
+  )
 }
 
 const Statistics = ({good, neutral, bad}) => {
@@ -29,18 +36,20 @@ const Statistics = ({good, neutral, bad}) => {
   if (n === 0) { return <p> No feedback given </p> }
 
   return (
-    <div>
-      <StatisticsLine text="good" value={good}/>
-      <StatisticsLine text="neutral" value={neutral}/>
-      <StatisticsLine text="bad" value={bad}/>
-      <StatisticsLine text="all" value={good + neutral + bad}/>
-      <StatisticsLine value={
-        <Average good={good} neutral={neutral} bad={bad}/>
-      }/>
-      <StatisticsLine value={
-        <Positive good={good} neutral={neutral} bad={bad}/>
-      }/>
-    </div>
+      <table>
+        <tbody>
+          <StatisticsLine text="good" value={good}/>
+          <StatisticsLine text="neutral" value={neutral}/>
+          <StatisticsLine text="bad" value={bad}/>
+          <StatisticsLine text="all" value={good + neutral + bad}/>
+          <StatisticsLine value={
+            <Average good={good} neutral={neutral} bad={bad}/>
+          }/>
+          <StatisticsLine value={
+            <Positive good={good} neutral={neutral} bad={bad}/>
+          }/>
+        </tbody>
+      </table>
   )
 }
 
