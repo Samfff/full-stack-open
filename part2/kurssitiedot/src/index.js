1d1a2c66a9bf4513e68af54e56d7454c65cf028c
Sam <samuel.sihvonen@aalto.fi>
2021-01-06 11:43:29 +0200

    2.4 done

--- part2/kurssitiedot/src/index.js
+++ part2/kurssitiedot/src/index.js
@@ -14,7 +14,7 @@ const Course = ({course}) => {
 const Header = ({course}) => {
   return (
     <div>
-      <h1>{course.name}</h1>
+      <h2>{course.name}</h2>
     </div>
   )
 }
@@ -30,7 +30,7 @@ const Part = ({part, exercises}) => {
 const Total = ({course}) => {
   const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
   return (
-    <p> Total of {total} exercises </p>
+    <h3> Total of {total} exercises </h3>
   )
 }
 
@@ -44,31 +44,56 @@ const Content = ({course}) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course}/>
+      <h1> Web Development Curriculum </h1>
+      <Course course={courses[0]}/>
+      <Course course={courses[1]}/>
     </div>
   )
 }
