aa5eaaa31e064dc96d2586647bb144be4e2d31f9
Sam <samuel.sihvonen@aalto.fi>
2021-01-08 01:32:55 +0200

    reinstalling maiden tiedot

--- part2/maiden-tiedot/src/components/Filter.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Filter = ({filter, handleFilterChanged}) => {
-  return (
-    <p>filter shown with <input value={filter} onChange={handleFilterChanged}/></p>
-  )
-}
-
-export default Filter
